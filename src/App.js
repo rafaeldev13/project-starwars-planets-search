4fd277a13bef5ab7475345f0a5f8d499dbfc5ae5
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-08-17 22:14:54 -0400

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Table from './components/table';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <>
+      <span>Hello, App!</span>
+      <Table />
+    </>
   );
 }
 
