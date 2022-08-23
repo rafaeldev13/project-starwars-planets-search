049b0a6f854d1ebbc51c6b2bf12c789acdfa181f
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-08-23 10:12:26 -0400

    Requisito 05

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import MyProvider from '../context/MyProvider'
 
-test('I am your test', () => {
+test('Check input elements', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+  const columnFilter = screen.getByTestId('column-filter');
+  expect(columnFilter).toBeInTheDocument();
+
+  const comparisonFilter = screen.getByTestId('comparison-filter');
+  expect(comparisonFilter).toBeInTheDocument();
+
+  const valueFilter = screen.getByTestId('value-filter');
+  expect(valueFilter).toBeInTheDocument();
+});
+
+test('Checks if the button is on the page', () => {
+  render(<App />);
+  const buttonFilter = screen.getByTestId('button-filter');
+  expect(buttonFilter).toBeInTheDocument();
+});
+
+it('Check the api return', async () => {
+  render(<MyProvider><App /></MyProvider>);
+  const planetName = await screen.findByText( /Alderaan/i);
+  expect(planetName).toBeInTheDocument();
 });
